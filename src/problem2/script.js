9597b7dd3f5f5009c4a33d1a368b62248fe9be0e
ScorpiusSigma <justinleng_@hotmail.com>
2023-02-17 00:41:13 +0800

    Add Javascript to handle interactions

---
+++ src/problem2/script.js
@@ -0,0 +1,71 @@
+const onSubmit = (e) => {};
+
+document
+	.getElementById("transaction-form")
+	.addEventListener("submit", function (e) {
+		e.preventDefault();
+
+		const data = [
+			...e.target.children[0].getElementsByTagName("input"),
+		].map((x) => x.value);
+
+		const address = data[0];
+		const amount = data[1];
+
+		e.target.children[1].children[0].innerHTML = "LOADING...";
+		setTimeout(() => {
+			e.target.children[1].children[0].innerHTML = "TOKEN SENT";
+			document.getElementById("transaction-alert").innerHTML =
+				amount + " token sent to " + address;
+
+			setTimeout(() => {
+				e.target.children[1].children[0].innerHTML = "SEND TOKENS";
+			}, 2000);
+		}, 2000);
+	});
+
+let otpClicked = false;
+const handleOtpButtonClick = (e) => {
+	if (otpClicked) {
+		return;
+	} else {
+		otpClicked = true;
+	}
+
+	e.target.innerHTML = "SENT!";
+
+	let countDown = 30;
+	const counter = () => {
+		e.target.innerHTML = countDown + "s";
+		countDown--;
+		if (countDown <= 0) {
+			clearInterval(interval);
+			e.target.innerHTML = "Send OTP";
+			otpClicked = false;
+		}
+	};
+
+	const interval = setInterval(counter, 1000);
+	countDown = 30;
+};
+
+document
+	.getElementById("otp-button")
+	.addEventListener("click", handleOtpButtonClick);
+
+const otpChildren = [...document.getElementById("digit-group").children];
+
+otpChildren.forEach((otp, index) => {
+	otp.addEventListener("keyup", (e) => {
+		if (e.keyCode >= 48 && e.keyCode <= 57 && e.target.value.length <= 1) {
+			const len = otpChildren.length;
+			const nextIndex = index + 1 >= len ? len - 1 : index + 1;
+			otpChildren[nextIndex].select();
+		} else if (e.keyCode === 8) {
+			const nextIndex = index - 1 < 0 ? 0 : index - 1;
+			otpChildren[nextIndex].select();
+		} else {
+			e.target.value = e.target.value[0];
+		}
+	});
+});
