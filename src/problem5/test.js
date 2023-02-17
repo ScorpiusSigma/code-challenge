0e28526a54aef8268c0815685d07f1ced3ff2b20
ScorpiusSigma <justinleng_@hotmail.com>
2023-02-18 01:19:57 +0800

    Deploy and test Utility contract

---
+++ src/problem5/test.js
@@ -0,0 +1,60 @@
+const { ethers } = require("ethers");
+
+const ADDR = "0x7731238D0ebC1EA41225e446F994a7ce76a3fD72"; // your contract address
+const ABI = [
+	{
+		inputs: [
+			{
+				internalType: "address",
+				name: "_walletAddress",
+				type: "address",
+			},
+			{
+				internalType: "address[]",
+				name: "_tokenContractAddresses",
+				type: "address[]",
+			},
+		],
+		name: "getBalances",
+		outputs: [
+			{
+				components: [
+					{
+						internalType: "address",
+						name: "token",
+						type: "address",
+					},
+					{
+						internalType: "uint256",
+						name: "balance",
+						type: "uint256",
+					},
+				],
+				internalType: "struct Utility.TokenAmountObject[]",
+				name: "",
+				type: "tuple[]",
+			},
+		],
+		stateMutability: "view",
+		type: "function",
+	},
+]; // your contract ABI
+
+let ADDRESS = "0xF9c350Ec1f67c9D548Aec60D528d6e0A0C97c517"; // some wallet address with token balance
+const TOKENS = [
+	// token contract addresses
+	"0x86718575B0D9d5a71c1372E6Bd195D8e795B60ED",
+];
+
+// you can use your own RPC provider url (no need to deploy to mainnet)
+const provider = ethers.providers.getDefaultProvider("goerli");
+
+const test = async () => {
+	const contract = new ethers.Contract(ADDR, ABI, provider);
+
+	const balances = await contract.getBalances(ADDRESS, TOKENS);
+
+	return balances;
+};
+
+test().then(console.log);
